aa14db700c144b27df4f92434b7d752a39a1706b
reset4ll <reset4ll@yahoo.es>
2022-04-19 20:40:42 +0200

    "update"

--- ch05/ex514.c
+++ ch05/ex514.c
@@ -9,28 +9,24 @@ int main(void)
 	printf("Introduzca la nota en número; ");
 	scanf("%3d", &numero);
 
-	/* Error? */
+	/* Check error */
 	if (numero >= 100 || numero <= 0) {
 		printf("Error, pruebe otra vez\n");
 		return -1;
 	}
 
-	/* Algorítmo para la evaluación -> dígito de la decena */
+	/* Algoritmo para obtener el dígito de la decena */
 	resultado = numero / 10;
 
 	/* Test */
 	switch (resultado) {
 
 	case 9: printf("La letra de la nota es A\n"); break;
-
 	case 8: printf("La letra de la nota es B\n"); break;
-
 	case 7: printf("La letra de la nota es C\n"); break;
-
 	case 6: printf("La letra de la nota es D\n"); break;
-
 	case 5: case 4: case 3: case 2: case 1:
-		printf("La letra de la nota es F\n"); break;
+		    printf("La letra de la nota es F\n"); break;
 
 	/* ¿Nota por debajo de 10? */
 	default: printf("La letra de la nota es F\n"); break;
