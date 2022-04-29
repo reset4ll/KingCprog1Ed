741a560b8481a31fe8ba79369381196d21c1fda5
reset4ll <reset4ll@yahoo.es>
2022-04-29 12:28:46 +0200

    subido ex81

---
+++ ch08/ex81.c
@@ -0,0 +1,43 @@
+/* Ejercicio 8.1 */
+
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int Bool; /* Nuevo tipo booleano */
+
+int main(void)
+{
+	int digito, index;
+	long int numero;
+
+    /* Arrays utilizados */
+	Bool digito_visto[10] = {0};
+	Bool es_repetido[10] = {0};
+
+	printf("Introduzca un número: ");
+	scanf("%ld", &numero);
+
+    /* Comienzo de la búsqueda*/
+	while (numero > 0) {
+		digito = numero % 10; /* Resto */
+		if (digito_visto[digito])
+			es_repetido[digito] = TRUE; /* Está repetido */
+		digito_visto[digito] = TRUE;
+		numero = numero / 10; /* Siguiente dígito */
+	}
+
+	printf("Dígitos repetido(s): ");
+
+    /* Búsqueda de los números repetidos */
+	for (index = 0; index < 10; index++) {
+        /*  Si es verdad que está repetido, imprímelo */
+		if (es_repetido[index])
+			printf("%d ", index);
+	}
+    printf("\n");
+
+	return 0;
+}
+
