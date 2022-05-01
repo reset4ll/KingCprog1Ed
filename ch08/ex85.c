0d65d3dc1bd38061ae04bfeb021abaab2b3a48c1
reset4ll <reset4ll@yahoo.es>
2022-05-01 17:24:03 +0200

    subido ex85

---
+++ ch08/ex85.c
@@ -0,0 +1,27 @@
+/* Ejercicio 8.5 */
+
+#include <stdio.h>
+
+/* Macro que define la longitud del arrayay */
+#define SIZE sizeof(array) / sizeof(array[0]) /* 'Unsigned long' */
+/* Número de elementos */
+#define N 10
+
+int main(void)
+{
+	int array[N] = {0};
+	int index;
+
+	printf("Introduzca %d números: ", N);
+	for (index = 0; index < SIZE; index++)
+		scanf("%d", &array[index]);
+
+    /* printf("La longitud del arrayay es: (%lu)\n", SIZE); */
+	printf("En orden inverso:     ");
+	for (index = SIZE - 1; index >= 0; index--)
+		printf(" %d", array[index]);
+
+	printf("\n");
+
+	return 0;
+}
