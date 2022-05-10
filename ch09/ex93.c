38e6767a8e162e64f230eef5a42656f0bf537b29
reset4ll <reset4ll@yahoo.es>
2022-05-10 21:34:01 +0200

    subido ex93

---
+++ ch09/ex93.c
@@ -0,0 +1,36 @@
+/* Ejercicio 9.3 */
+
+#include <stdio.h>
+
+/* Prototipo */
+void mcd(int, int);
+
+int main(void)
+{
+	int dividendo, divisor;
+
+	printf("Introducir dos números enteros no negativos: \n");
+	printf("Introduce el dividendo: ");
+	scanf("%d", &dividendo);
+	printf("Introducir el divisor: ");
+	scanf("%d", &divisor);
+
+	/* Máximo común divisor de dos números enteros */
+	mcd(dividendo, divisor);
+
+	return 0;
+}
+
+/* Definición de la función */
+void mcd(int dividendo, int divisor)
+{
+	int resto = 0;
+	resto = dividendo % divisor; /* Inicialización */
+	while (resto != 0) {
+		/* Declaración */
+		dividendo = divisor;
+		divisor = resto;
+		resto = dividendo % divisor;
+	}
+	printf("El mcd de los dos números es: %d\n", divisor);
+}
